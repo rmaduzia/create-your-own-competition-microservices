26c5bef20657413438c548cfd402aa374b2ea871
rmaduzia <rafi11m@hotmail.com>
2024-06-05 20:46:51 +0200

    tournament service - adding unit tests

---
+++ tournament-service/src/test/java/pl/createcompetition/tournamentservice/service/MatchInTournamentServiceTest.java
@@ -0,0 +1,114 @@
+package pl.createcompetition.tournamentservice.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import pl.createcompetition.tournamentservice.all.tournament.Tournament;
+import pl.createcompetition.tournamentservice.all.tournament.TournamentRepository;
+import pl.createcompetition.tournamentservice.all.tournament.match.MatchInTournament;
+import pl.createcompetition.tournamentservice.all.tournament.match.MatchInTournamentRepository;
+import pl.createcompetition.tournamentservice.all.tournament.match.MatchInTournamentService;
+import pl.createcompetition.tournamentservice.microserviceschanges.UserPrincipal;
+
+
+@ExtendWith(MockitoExtension.class)
+public class MatchInTournamentServiceTest {
+
+    @Mock
+    MatchInTournamentRepository matchInTournamentRepository;
+    @Mock
+    TournamentRepository tournamentRepository;
+    @InjectMocks
+    MatchInTournamentService matchInTournamentService;
+
+    @Mock
+    UserPrincipal userPrincipal;
+
+
+    Tournament tournament;
+    MatchInTournament matchInTournament;
+
+    private static final String userName = "someUserName";
+
+    @BeforeEach
+    public void setUp() {
+
+        when(userPrincipal.getName()).thenReturn(userName);
+
+        tournament = Tournament.builder()
+                .id(1L)
+                .maxAmountOfTeams(10)
+                .tournamentOwner(userName)
+                .tournamentName("Tourtnament1").build();
+
+        matchInTournament = MatchInTournament.builder()
+                .id(1L)
+                .firstTeamName("firstTeam")
+                .secondTeamName("secondTeam")
+                .isMatchWasPlayed(false)
+                .isWinnerConfirmed(false)
+                .build();
+
+        matchInTournament.addMatchToTournament(tournament);
+    }
+
+    @Test
+    public void shouldAddMatchInTournament() {
+
+        when(tournamentRepository.findByTournamentNameAndTournamentOwner(tournament.getTournamentName(), tournament.getTournamentOwner())).thenReturn(
+            Optional.of(tournament));
+        when(matchInTournamentRepository.save(matchInTournament)).thenReturn(matchInTournament);
+
+        ResponseEntity<?> response = matchInTournamentService.addMatchInTournament(matchInTournament, tournament.getTournamentName(), userPrincipal);
+
+        verify(tournamentRepository, times(1)).findByTournamentNameAndTournamentOwner(tournament.getTournamentName(), tournament.getTournamentOwner());
+        verify(matchInTournamentRepository, times(1)).save(matchInTournament);
+
+        assertEquals(response.getStatusCode(), HttpStatus.CREATED);
+        assertEquals(response.getBody(), matchInTournament);
+    }
+
+    @Test
+    public void shouldUpdateMatchInTournament() {
+
+        when(matchInTournamentRepository.existsById(matchInTournament.getId())).thenReturn(true);
+        when(tournamentRepository.findByTournamentNameAndTournamentOwner(tournament.getTournamentName(), tournament.getTournamentOwner())).thenReturn(
+            Optional.of(tournament));
+        when(matchInTournamentRepository.save(matchInTournament)).thenReturn(matchInTournament);
+
+        matchInTournament.setIsMatchWasPlayed(true);
+
+        ResponseEntity<?> response = matchInTournamentService.updateMatchInTournament(matchInTournament, matchInTournament.getId(), userPrincipal);
+
+        verify(matchInTournamentRepository, times(1)).existsById(matchInTournament.getId());
+        verify(tournamentRepository, times(1)).findByTournamentNameAndTournamentOwner(tournament.getTournamentName(), tournament.getTournamentOwner());
+        verify(matchInTournamentRepository, times(1)).save(matchInTournament);
+
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+        assertEquals(response.getBody(), matchInTournament);
+    }
+
+    @Test
+    public void shouldDeleteMatchInTournament() {
+
+        when(matchInTournamentRepository.findById(matchInTournament.getId())).thenReturn(Optional.of(matchInTournament));
+
+        ResponseEntity<?> response = matchInTournamentService.deleteMatchInTournament(matchInTournament.getId(), userPrincipal);
+
+        verify(matchInTournamentRepository, times(1)).findById(matchInTournament.getId());
+        verify(matchInTournamentRepository, times(1)).deleteById(matchInTournament.getId());
+
+        assertEquals(response.getStatusCode(), HttpStatus.NO_CONTENT);
+    }
+}
---
+++ tournament-service/src/test/java/pl/createcompetition/tournamentservice/util/MatchTeamsInTournamentTest.java
@@ -0,0 +1,49 @@
+package pl.createcompetition.tournamentservice.util;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import org.junit.jupiter.api.Test;
+
+public class MatchTeamsInTournamentTest {
+
+    @Test
+    public void matchTeamsInTournament() {
+
+        List<String> listOfTeams = new ArrayList<>();
+        listOfTeams.add("Team1");
+        listOfTeams.add("Team2");
+
+        Map<String,String> exceptedMatchedTeams1 = new TreeMap<>();
+        exceptedMatchedTeams1.put("A", "Team1 VS Team2");
+        Map<String,String> exceptedMatchedTeams2 = new TreeMap<>();
+        exceptedMatchedTeams2.put("A", "Team2 VS Team1");
+
+        Map<String,String> matchedTeams = MatchTeamsInTournament.matchTeamsInTournament(listOfTeams);
+
+        assertTrue(matchedTeams.equals(exceptedMatchedTeams1) || matchedTeams.equals(exceptedMatchedTeams2));
+
+    }
+
+    @Test
+    public void matchTeamsWithEachOtherInTournament() {
+
+        List<String> listOfTeams = new ArrayList<>();
+        listOfTeams.add("Team1");
+        listOfTeams.add("Team2");
+        listOfTeams.add("Team3");
+
+        Map<String,String> exceptedMatchedTeams = new TreeMap<>();
+        exceptedMatchedTeams.put("0", "Team1 VS Team2");
+        exceptedMatchedTeams.put("1", "Team1 VS Team3");
+        exceptedMatchedTeams.put("2", "Team2 VS Team3");
+
+        Map<String,String> matchedTeams = MatchTeamsInTournament.matchTeamsWithEachOtherInTournament(listOfTeams);
+
+        assertEquals(matchedTeams, exceptedMatchedTeams);
+    }
+}
