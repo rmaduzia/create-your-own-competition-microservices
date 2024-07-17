d81be962f8026e8e5a7b03c0f5a0c5b4a82f396f
rmaduzia <rafi11m@hotmail.com>
2024-07-17 20:14:19 +0200

    tournament-competition service - config - changed max mount of teams in competition

--- tournament-service/src/main/java/pl/createcompetition/tournamentservice/config/AppConstants.java
+++ tournament-service/src/main/java/pl/createcompetition/tournamentservice/config/AppConstants.java
@@ -7,7 +7,7 @@ public final class AppConstants {
     public static final int MAX_NUMBER_OF_TOURNAMENT_TO_CREATE = 5;
     public static final int MAX_AMOUNT_OF_TEAMS_IN_TOURNAMENT = 5;
     public static final int MAX_AMOUNT_OF_USERS_IN_TEAM = 10;
-    public static final int MAX_AMOUNT_OF_TEAMS_IN_COMPETITION = 5;
+    public static final int MAX_AMOUNT_OF_TEAMS_IN_COMPETITION = 30;
     public static final int MINIMUM_AMOUNT_OF_TEAMS_IN_TOURNAMENT = 2;
     public static final int MINIMUM_AMOUNT_OF_TEAMS_IN_COMPETITION = 2;
 
