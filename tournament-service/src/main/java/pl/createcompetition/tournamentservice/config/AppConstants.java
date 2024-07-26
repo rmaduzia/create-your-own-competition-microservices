1848ca38cdb38a52c37437cd396f533f5d5cfc89
rmaduzia <rafi11m@hotmail.com>
2024-07-26 15:38:35 +0200

    tournament-competition service - change config for max amount of teams in tournament

--- tournament-service/src/main/java/pl/createcompetition/tournamentservice/config/AppConstants.java
+++ tournament-service/src/main/java/pl/createcompetition/tournamentservice/config/AppConstants.java
@@ -5,7 +5,7 @@ public final class AppConstants {
     public static final int MAX_NUMBER_OF_TEAM_TO_CREATE = 5;
     public static final int MAX_NUMBER_OF_COMPETITIONS_TO_CREATE = 5;
     public static final int MAX_NUMBER_OF_TOURNAMENT_TO_CREATE = 5;
-    public static final int MAX_AMOUNT_OF_TEAMS_IN_TOURNAMENT = 5;
+    public static final int MAX_AMOUNT_OF_TEAMS_IN_TOURNAMENT = 30;
     public static final int MAX_AMOUNT_OF_USERS_IN_TEAM = 10;
     public static final int MAX_AMOUNT_OF_TEAMS_IN_COMPETITION = 30;
     public static final int MINIMUM_AMOUNT_OF_TEAMS_IN_TOURNAMENT = 2;
