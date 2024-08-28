58db7ee178f89942fba23f9c506ab51c130e5e06
rmaduzia <rafi11m@hotmail.com>
2024-08-28 22:07:08 +0200

    tournament-competition service - competition tag service unit tests

---
+++ tournament-service/src/test/java/pl/createcompetition/tournamentservice/service/CompetitionTagServiceTest.java
@@ -0,0 +1,94 @@
+package pl.createcompetition.tournamentservice.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.lang.module.ResolutionException;
+import java.sql.Timestamp;
+import java.util.Optional;
+import java.util.Set;
+import org.assertj.core.util.Sets;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mapstruct.factory.Mappers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.server.ResponseStatusException;
+import pl.createcompetition.tournamentservice.competition.Competition;
+import pl.createcompetition.tournamentservice.competition.CompetitionRepository;
+import pl.createcompetition.tournamentservice.competition.EventMapper;
+import pl.createcompetition.tournamentservice.competition.tag.CompetitionTagService;
+import pl.createcompetition.tournamentservice.microserviceschanges.UserPrincipal;
+import pl.createcompetition.tournamentservice.model.Tag;
+import pl.createcompetition.tournamentservice.tournament.VerifyMethodsForServices;
+
+
+@ExtendWith(MockitoExtension.class)
+public class CompetitionTagServiceTest {
+
+    @Mock
+    CompetitionRepository competitionRepository;
+    @InjectMocks
+    CompetitionTagService competitionTagService;
+
+    @Mock
+    VerifyMethodsForServices verifyMethodsForServices;
+
+    @Mock
+    UserPrincipal userPrincipal;
+
+    EventMapper eventMapper = Mappers.getMapper(EventMapper.class);
+
+    private static final String userName = "someUserName";
+
+    Competition competition;
+    Tag competitionTag;
+
+    @BeforeEach
+    public void setUp() {
+
+        when(userPrincipal.getName()).thenReturn(userName);
+
+        competition = Competition.builder()
+                .id(1L)
+                .eventOwner("test@mail.com")
+                .eventName("zawody1")
+                .eventStartDate(Timestamp.valueOf("2020-05-01 12:30:00").toLocalDateTime())
+                .eventEndDate(Timestamp.valueOf("2020-05-02 12:30:00").toLocalDateTime())
+                .city("Gdynia")
+                .maxAmountOfTeams(10)
+                .tags(Sets.newHashSet())
+                .build();
+
+        competitionTag = Tag.builder().tag("Tag").id(1L).competitions(Sets.newHashSet()).build();
+    }
+
+    @Test
+    public void shouldAddTags() {
+
+        when(competitionRepository.save(competition)).thenReturn(competition);
+
+        when(verifyMethodsForServices.shouldFindCompetition(competition.getEventName())).thenReturn(competition);
+
+
+        Set<Tag> tags = Set.of(competitionTag);
+        ResponseEntity<?> response = competitionTagService.addCompetitionTag(tags, competition.getEventName(), userPrincipal.getName());
+
+        verify(competitionRepository, times(1)).save(competition);
+
+        assertEquals(response.getStatusCode(), HttpStatus.CREATED);
+        assertEquals(response.getBody(), competition);
+    }
+
+}
