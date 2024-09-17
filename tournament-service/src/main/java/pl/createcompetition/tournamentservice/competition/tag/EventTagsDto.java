59215a0451bbbb2a5fade1c21c5833a710d553aa
rmaduzia <rafi11m@hotmail.com>
2024-09-17 18:39:17 +0200

    tournament-competition service - update competition tag tests

--- tournament-service/src/main/java/pl/createcompetition/tournamentservice/competition/tag/EventTagsDto.java
+++ tournament-service/src/main/java/pl/createcompetition/tournamentservice/competition/tag/EventTagsDto.java
@@ -2,9 +2,11 @@ package pl.createcompetition.tournamentservice.competition.tag;
 
 import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 @AllArgsConstructor
 public class EventTagsDto {
 
--- tournament-service/src/test/java/pl/createcompetition/tournamentservice/service/CompetitionTagServiceTest.java
+++ tournament-service/src/test/java/pl/createcompetition/tournamentservice/service/CompetitionTagServiceTest.java
@@ -1,17 +1,14 @@
 package pl.createcompetition.tournamentservice.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.lang.module.ResolutionException;
 import java.sql.Timestamp;
+import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 import org.assertj.core.util.Sets;
@@ -21,8 +18,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mapstruct.factory.Mappers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.server.ResponseStatusException;
@@ -30,26 +27,27 @@ import pl.createcompetition.tournamentservice.competition.Competition;
 import pl.createcompetition.tournamentservice.competition.CompetitionRepository;
 import pl.createcompetition.tournamentservice.competition.EventMapper;
 import pl.createcompetition.tournamentservice.competition.tag.CompetitionTagService;
+import pl.createcompetition.tournamentservice.competition.tag.EventTagsDto;
 import pl.createcompetition.tournamentservice.microserviceschanges.UserPrincipal;
 import pl.createcompetition.tournamentservice.model.Tag;
 import pl.createcompetition.tournamentservice.tournament.VerifyMethodsForServices;
-
 
 @ExtendWith(MockitoExtension.class)
 public class CompetitionTagServiceTest {
 
     @Mock
-    private CompetitionRepository competitionRepository;
+    CompetitionRepository competitionRepository;
 
-    @Mock
-    private VerifyMethodsForServices verifyMethodsForServices;
+    CompetitionTagService competitionTagService;
 
     @InjectMocks
-    private CompetitionTagService competitionTagService;
+    VerifyMethodsForServices verifyMethodsForServices;
+
 
     @Mock
-    UserPrincipal userPrincipal;
+    private UserPrincipal userPrincipal;
 
+    @Spy
     EventMapper eventMapper = Mappers.getMapper(EventMapper.class);
 
     private static final String userName = "someUserName";
@@ -60,11 +58,13 @@ public class CompetitionTagServiceTest {
     @BeforeEach
     public void setUp() {
 
+        competitionTagService = new CompetitionTagService(competitionRepository, verifyMethodsForServices);
+
         when(userPrincipal.getName()).thenReturn(userName);
 
         competition = Competition.builder()
                 .id(1L)
-                .eventOwner("test@mail.com")
+                .eventOwner(userName)
                 .eventName("zawody1")
                 .eventStartDate(Timestamp.valueOf("2020-05-01 12:30:00").toLocalDateTime())
                 .eventEndDate(Timestamp.valueOf("2020-05-02 12:30:00").toLocalDateTime())
@@ -79,35 +79,43 @@ public class CompetitionTagServiceTest {
     @Test
     public void shouldAddTags() {
 
-        when(competitionRepository.save(competition)).thenReturn(competition);
+        when(competitionRepository.findByEventName(competition.getEventName())).thenReturn(
+            Optional.ofNullable(competition));
 
-        when(verifyMethodsForServices.shouldFindCompetition(competition.getEventName())).thenReturn(competition);
-
-
-        Set<Tag> tags = Set.of(competitionTag);
+        Set<String> tags = new HashSet<>(Set.of("someTag", "someNextTag"));
         ResponseEntity<?> response = competitionTagService.addCompetitionTag(tags, competition.getEventName(), userPrincipal.getName());
 
         verify(competitionRepository, times(1)).save(competition);
 
+        EventTagsDto returnedEventTagsDto = (EventTagsDto) response.getBody();
+
         assertEquals(response.getStatusCode(), HttpStatus.CREATED);
-        assertEquals(response.getBody(), competition);
+        assert returnedEventTagsDto != null;
+        assertEquals(competition.getEventName(), returnedEventTagsDto.getEventName());
+        assertTrue(returnedEventTagsDto.getTags().containsAll(tags));
     }
 
     @Test
     public void shouldUpdateTag() {
 
-        when(verifyMethodsForServices.shouldFindCompetition(competition.getEventName())).thenReturn(competition);
+        when(competitionRepository.findByEventName(competition.getEventName())).thenReturn(
+            Optional.ofNullable(competition));
+
         when(competitionRepository.save(competition)).thenReturn(competition);
 
-        competitionTag.setTag("updatedTag");
+        String competitionTag = "updatedTag";
         ResponseEntity<?> response = competitionTagService.updateCompetitionTag(competitionTag, competition.getEventName(), userPrincipal.getName());
 
         verify(competitionRepository, times(1)).save(competition);
 
-        assertEquals(response.getStatusCode(), HttpStatus.OK);
-        assertEquals(response.getBody(), competition);
-    }
+        EventTagsDto returnedEventTagsDto = (EventTagsDto) response.getBody();
 
+        assertEquals(response.getStatusCode(), HttpStatus.CREATED);
+        assert returnedEventTagsDto != null;
+        assertEquals(competition.getEventName(), returnedEventTagsDto.getEventName());
+        assertTrue(returnedEventTagsDto.getTags().contains(competitionTag));
+
+    }
 
     @Test
     public void shouldThrowExceptionCompetitionNotExistsWhenAddTag() {
@@ -120,8 +128,10 @@ public class CompetitionTagServiceTest {
             "Expected doThing() to throw, but it didn't");
 
         verify(competitionRepository, times(1)).findByEventName(competition.getEventName());
-        assertEquals("Competition not exists not found with Name : '"+ competition.getEventName()+ "'", exception.getMessage());
+        assertEquals(HttpStatus.NOT_FOUND, exception.getStatusCode());
+        assertEquals("Competition not exists, Name: "+ competition.getEventName(), exception.getReason());
     }
+
 
 
 }
