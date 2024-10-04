57039d5d8976ff5a7a5cc1f977ee1d1472395206
rmaduzia <rafi11m@hotmail.com>
2024-10-04 18:37:37 +0200

    tournament-competition service - update verify methods

--- tournament-service/src/main/java/pl/createcompetition/tournamentservice/tournament/VerifyMethodsForServices.java
+++ tournament-service/src/main/java/pl/createcompetition/tournamentservice/tournament/VerifyMethodsForServices.java
@@ -1,6 +1,6 @@
 package pl.createcompetition.tournamentservice.tournament;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -11,14 +11,22 @@ import pl.createcompetition.tournamentservice.competition.CompetitionRepository;
 
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class VerifyMethodsForServices {
 
     private final CompetitionRepository competitionRepository;
     private final FindTeamService findTeamService;
+    private final TournamentRepository tournamentRepository;
 
     public TeamDto shouldFindTeam(String teamName, String teamOwner) {
-        return findTeamService.findTeam(teamName, teamOwner);
+
+        TeamDto foundTeam = findTeamService.findTeam(teamName, teamOwner);
+
+        if (foundTeam == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Team not found, Team Name: " + teamName + " Team owner: " + teamOwner);
+        }
+
+        return foundTeam;
     }
 
     public TeamDto shouldFindTeam(String teamName) {
@@ -28,6 +36,11 @@ public class VerifyMethodsForServices {
     public Competition shouldFindCompetition(String competitionName) {
         return competitionRepository.findByEventName(competitionName).orElseThrow(() ->
                 new ResponseStatusException(HttpStatus.NOT_FOUND, "Competition not exists, Name: " +competitionName));
+    }
+
+    public Tournament shouldFindTournament(String competitionName) {
+        return tournamentRepository.findByEventName(competitionName).orElseThrow(() ->
+            new ResponseStatusException(HttpStatus.NOT_FOUND, "Competition not exists, Name: " +competitionName));
     }
 
     public void checkIfCompetitionBelongToUser(String competitionName, String userName) {
